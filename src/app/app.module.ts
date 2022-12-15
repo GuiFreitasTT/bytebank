d4c16a30faabab3bd5c225ca6b4cf2986ceebaa0
GuiFreitasTT <guidiegofreitas@yahoo.com.br>
2022-12-15 20:41:36 -0300

    -m "first commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from "./nova-transferencia/nova-transferencia.component";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NovaTransferenciaComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ]
 })
 export class AppModule { }
